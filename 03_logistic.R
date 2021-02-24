f851a70afd0bbcd8b174540c5f12159904a56914
Devin Hubbard <devinhubbard@Devins-MacBook-Pro.local>
2021-02-23 19:44:34 -0500

    Owner step 4!

--- 03_logistic.R
+++ 03_logistic.R
@@ -1,5 +1,4 @@
-```{r}
-##' Logistic model
+Logistic model
 ##'
 ##' @param theta  parameter vector
 ##' @param x      vector of x values
@@ -8,7 +7,6 @@ pred.logistic <- function(theta,x){
   z = exp(theta[3]+theta[4]*x)
   Ey = theta[1]+theta[2]*z/(1+z) 
 }
-
 ##' Fit logistic model
 ##' 
 ##' @param dat  dataframe of day of year (doy), gcc_mean, gcc_std
@@ -18,11 +16,9 @@ fit.logistic <- function(dat,par){
   
   ## define log likelihood
   lnL.logistic <- function(theta,dat){
-    -sum(dnorm(dat$gcc_mean,pred.logistic(theta,dat$doy),dat$gcc_std,log=TRUE))
+    -sum(dnorm(dat$gcc_mean,pred.logistic(theta,dat$doy),dat$gcc_std,log=TRUE),na.rm=TRUE)
   }
   
   ## fit by numerical optimization
   optim(par,fn = lnL.logistic,dat=dat)
 }
-
-```
